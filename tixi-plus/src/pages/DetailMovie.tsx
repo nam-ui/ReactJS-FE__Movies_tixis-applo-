3caacd31e64ff78d789541f2596842c10ee59d1b
nam-ui <anhvipkaka9999@gmail.com>
2021-04-23 14:50:37 +0700

    movie tsx

--- tixi-plus/src/pages/DetailMovie.tsx
+++ tixi-plus/src/pages/DetailMovie.tsx
@@ -1,6 +1,9 @@
 import { useQuery } from '@apollo/client';
 import React from 'react'
+import { BiShareAlt } from 'react-icons/bi';
+import { FiClock } from 'react-icons/fi';
 import { useParams } from 'react-router-dom'
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import { movieQuery } from '../graphql/Movie';
 import { UserType } from '../models/UserType';
@@ -20,6 +23,10 @@ function DetailMovie() {
         setAccount(user)
     }, [])
 
+    const time = (time: any) => {
+        const timeNow = new Date(parseInt(time.toString())).toISOString().slice(0, 4)
+        return timeNow
+    }
 
     const { loading, error, data } = useQuery(movieQuery, {
         variables: {
@@ -28,17 +35,47 @@ function DetailMovie() {
     })
     if (loading) return <p>Loading...</p>
     if (error) return <p>error...</p>
-    
+
+    console.log(data);
+
+
     return (
         <React.Fragment>
-            <header  style={{backgroundImage:`url(${banner})`}} >
+            <header style={{ backgroundImage: `url(${banner})` }} >
                 <Header user={account} />
                 <div id="header-detail-movie" >
-                    <img src={data.movie.picture} alt=""/>
-                    <h2 style={{color: "red"}}>{data.movie.moviesName}</h2>
-                    <iframe width="560" height="315" src={data.movie.trailer} title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <div id="poster-movie-detail">
+                        <a href="#" className="open-movie"></a>
+                        <img src={data.movie.picture} alt="poster-movie" />
+                    </div>
+                    <div id="information-movie-detail">
+                        <p><h2 className="episodes">{"New Episodes"}</h2></p>
+                        <p><h2  className="movie-detail-name">{data.movie.moviesName}</h2></p>
+                        <p>  <span className="date">{time(data.movie.launchDate)}</span><span className="duration"><FiClock ></FiClock> 128 min</span> </p>
+                        <p>
+                            <span className="described">
+                                {data.movie.described}
+                            </span>
+                        </p>
+                        <div className="btn-share">
+                            <button className="btn-share-left" >
+                                < BiShareAlt fontSize="25px" />
+                                <p>Share</p>
+                            </button>
+                            <div className="btn-share-right">
+                                <div>
+                                    <p>Prime Video </p>
+                                    <p>Streaming Channels</p>
+                                </div>
+                                <button>
+                                    Mua vé
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </header>
+            <Footer />
         </ React.Fragment>
     )
 }
